f31c6648dc1aa578832c551876566565824eec91
Alexis Bezverkhyy <alexis@grapsus.net>
2016-09-19 23:49:28 +0200

    init_clock() ok, timings now seem accurate

--- util.c
+++ util.c
@@ -4,24 +4,9 @@
 
 void init_clock()
 {
-    // OSC_PD=0: Power up both high-speed oscillators: XOSC and HS RCOSC.
-    SLEEP &= ~0x04;
-
-    // Wait until the high speed crystal oscillator is stable (SLEEP.XOSC_STB=1)
-    // This condition is required to use the radio or USB.
-    // According to Table 6.4.2, the waiting should take about 650 microseconds.
-    while(!(SLEEP & 0x40));
-
-    // OSC32K = 1: Use low power 32kHz oscillator for the 32 kHz signal.
-    // OSC=0: Select high speed crystal oscillator for system clock (24 MHz).
-    // TICKSPD=000: Set the timer tick speed to its fastest possible value.
-    // CLKSPD=000: Set system clock speed to its fastest possible value (24 MHz).
-    //    This is required for using the Forward Error Correction radio feature (which we don't use anymore).
-    CLKCON = 0x80;
-
-    // Power down the HS RCOSC (the one that is not currently selected by
-    // CLKCON.OSC).
-    SLEEP |= 0x04;
+    // run both external oscillators and system at full speed
+    CLKCON = 0x00;
+    SLEEP = 0x84;
 
     // Enable pre-fetching of instructions from flash,
     // which makes the code execute much faster.
