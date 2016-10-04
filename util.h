3aed257a99d6bbdc54cc40405d811a70cf92ecff
Alexis Bezverkhyy <alexis@grapsus.net>
2016-10-04 23:22:37 +0200

    make sure the interrupt table has the right length to prevent weird things from happening

--- util.h
+++ util.h
@@ -6,3 +6,7 @@ void delay_ms(uint16 milliseconds);
 void disableUsbPullup();
 void enableUsbPullup();
 uint8 vinPowerPresent();
+/* even if we don't use this interrupt, make sure we have a vector for it
+   so that the interrupt table has at least 17 entries, otherwise SDCC makes
+   a sorter table and places code just after it :s */
+void isr_wdt() __interrupt(17);
